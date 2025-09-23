b2f831a4b1cdca6a8e7f5b3f693355c49ff86a4b
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-09-23 15:38:48 +0300

    Implementing getTotalGradeAnalytics Funtion.

---
+++ utils/dist/analytics/subjects.js
@@ -0,0 +1,24 @@
+export function getTotalGradeAnalytics(students) {
+    const totalGradesList = [];
+    // Loop on students' List
+    students.forEach((student) => {
+        const studentTotalGrade = student.student_totalgrade;
+        // Get Index of student's total grade in totalGradesList
+        const totalGradeIndex = totalGradesList.findIndex((item) => {
+            return item.grade_name === studentTotalGrade;
+        });
+        // "-1" means that this total grade not founded in the totalGradesList
+        if (totalGradeIndex === -1) {
+            // When total grade was not found; add new object with this total grade
+            totalGradesList.push({
+                grade_name: studentTotalGrade,
+                grade_count: 1,
+            });
+            // When total grade founded; increase its grade_count
+        }
+        else {
+            totalGradesList[totalGradeIndex].grade_count += 1;
+        }
+    });
+    return totalGradesList;
+}
